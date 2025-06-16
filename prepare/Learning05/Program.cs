c0a3ca53510f4c76bd983df91133c21facceff41
Ethan Trent <ethanjotrent@gmail.com>
2025-06-16 12:52:53 -0600

    shapes program

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        // Create individual shapes and test them
+        Square square = new Square("Red", 5);
+        // Console.WriteLine($"Square - Color: {square.GetColor()}, Area: {square.GetArea()}");
+
+        Rectangle rectangle = new Rectangle("Blue", 4, 6);
+        // Console.WriteLine($"Rectangle - Color: {rectangle.GetColor()}, Area: {rectangle.GetArea()}");
+
+        Circle circle = new Circle("Green", 3);
+        // Console.WriteLine($"Circle - Color: {circle.GetColor()}, Area: {circle.GetArea()}");
+
+        // Create a list of shapes
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        // Iterate through the list and display each shape's information
+        Console.WriteLine("\nIterating through shapes list:");
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape - Color: {shape.GetColor()}, Area: {shape.GetArea()}");
+        }
     }
 }
